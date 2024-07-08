38e3a98be642e1840ad7433e53594f165d349c88
mikeian <zhouwenxiang@ciwei.net>
2024-07-08 11:49:22 +0800

    use pina

---
+++ vue-hello/src/components/MyStoreComp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        this is counter store component {{cc}}
+        <button @click='increment'>increment</button>
+    </div>
+</template>
+<script setup lang='ts'>
+import { useCounterStore } from '@/stores/counter' 
+import { computed } from 'vue';
+const store = useCounterStore()
+
+const {count, increment} = store;
+const cc = computed(()=>count)
+
+</script>
+<style >
+    
+</style>
